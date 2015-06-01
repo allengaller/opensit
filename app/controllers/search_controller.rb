81e173fccc911da785208c4f68ee06d684518883
Dan Bartlett <danbartlett@gmail.com>
2015-06-01 19:47:11 +0100

    Filter private content from user and sit searches

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,29 @@
 class SearchController < ApplicationController
   def main
-  	@users = User.fuzzy_search(params[:q]).paginate(:page => params[:page])
-  	@sits = Sit.basic_search(params[:q]).paginate(:page => params[:page])
-    if Tag.find_by_name(params[:q]) != nil
+    base_users = user_signed_in? ? current_user.viewable_users : User.public_users
+    @users = base_users.fuzzy_search(params[:q]).paginate(:page => params[:page])
+
+    base_sits = user_signed_in? ? Sit.content_i_can_view(current_user) : Sit.public_sits
+    @sits = Sit.content_i_can_view(current_user).basic_search(params[:q]).paginate(:page => params[:page])
+
+    if Tag.find_by_name(params[:q])
       @tagged = Sit.tagged_with(params[:q]).paginate(:page => params[:page])
-  	end
+    end
+
     @page_class = "search-results"
     @title = "Search: #{params[:q]}"
 
-  	type = params[:type]
+    type = params[:type]
 
-	  if !type
-  	 render 'sits'
+    if !type
+      render 'sits'
     elsif type == 'users'
-  		render 'users'
-  	elsif type == 'tagged'
+      render 'users'
+    elsif type == 'tagged'
       render 'tagged'
     else
-  		render 'sits'
-  	end
+      render 'sits'
+    end
   end
 
 end
